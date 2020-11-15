85e79a9991b381814ee2aae61a4d8a44b51cb89a
Erki Kanarbik <erkikanarbik@gmail.com>
2020-11-15 20:51:08 +0200

    2

--- src/api/FetchWeather.js
+++ src/api/FetchWeather.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=tallinn&appid=f33a484cf794d08d0148764789aaba32"; // site that doesn’t send Access-Control-*
 
 const FetchWeather = async () =>  {
-            const result = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=tallinn&appid=f33a484cf794d08d0148764789aaba32',
+            const result = await axios.get(proxyurl + url,
                 {
                     params: {
                         units: 'metric',
